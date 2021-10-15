fdb12f17dc6bf4ebfbc603ba7098a0b2f45f8f0d
{krism01} <{krism01@naver.com}>
2021-10-15 13:27:09 +0900

    게시물 axios 요청하여 가져오깅

--- vuestagram/src/App.vue
+++ vuestagram/src/App.vue
@@ -11,6 +11,11 @@
 
     <Container :post="post"/>
 
+    <!-- axios 요청하여 깃헙서버에서 json 가져오기 -->
+    <button class="btn btn-primary" @click="more">
+      게시글 더 보기
+    </button>
+
     <div class="footer">
       <ul class="footer-button-plus">
         <input type="file" id="file" class="inputfile" />
@@ -22,15 +27,30 @@
 <script>
 import Container from './components/Container.vue';
 import post from './assets/data/post';
+import axios from 'axios';
 
 export default {
   name: 'App',
   data () {
     return {
       post: post,
+      count: 0,
     }
   },  
   components: { Container, },
+  methods: {
+    more(){
+      // axios 요청하기
+      // 문자열 안에 변수 설정 방법(es6)
+      axios.get(`https://minjaehyun.github.io/vue/more${this.count++}.json`)
+      .then(result =>{
+        // console.log(result.data);
+        // 게시글 저장하기
+        this.post.push(result.data)
+      })
+      .catch()
+    }
+  }
 }
 </script>
 
