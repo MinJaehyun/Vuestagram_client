a8a912960f2892b44f8f22dde61c3e5792f29259
{krism01} <{krism01@naver.com}>
2021-10-15 14:50:57 +0900

    메인페이지,필터기능,텍스트기능 구분하기 위한 UI 기본 설정

--- vuestagram/src/App.vue
+++ vuestagram/src/App.vue
@@ -1,59 +1,64 @@
 <template>
+  <!-- header -->
   <div class="header">
-      <ul class="header-button-left">
-        <li>Cancel</li>
-      </ul>
-      <ul class="header-button-right">
-        <li>Next</li>
-      </ul>
-      <img src="./assets/logo.png" class="logo" />
-    </div>
-
-    <Container :post="post"/>
-
-    <!-- axios 요청하여 깃헙서버에서 json 가져오기 -->
-    <button class="btn btn-primary" @click="more">
-      게시글 더 보기
-    </button>
-
-    <div class="footer">
-      <ul class="footer-button-plus">
-        <input type="file" id="file" class="inputfile" />
-        <label for="file" class="input-plus">+</label>
-      </ul>
+    <ul class="header-button-left">
+      <li>Cancel</li>
+    </ul>
+    <ul class="header-button-right">
+      <li>Next</li>
+    </ul>
+    <img src="./assets/logo.png" class="logo" />
   </div>
+
+  <!-- component -->
+  <Container :post="post" :step="step"/>
+
+  <!-- axios 요청하여 깃헙서버에서 json 가져오기 -->
+  <button class="btn btn-primary" @click="more">
+    게시글 더 보기
+  </button>
+
+  <!-- footer -->
+  <div class="footer">
+    <ul class="footer-button-plus">
+      <input type="file" id="file" class="inputfile" />
+      <label for="file" class="input-plus">+</label>
+    </ul>
+  </div>
+
 </template>
 
 <script>
-import Container from './components/Container.vue';
-import post from './assets/data/post';
-import axios from 'axios';
+import Container from "./components/Container.vue";
+import post from "./assets/data/post";
+import axios from "axios";
 
 export default {
-  name: 'App',
-  data () {
+  name: "App",
+  data() {
     return {
+      step: 0,
       post: post,
       count: 0,
-    }
-  },  
-  components: { Container, },
+    };
+  },
+  components: { Container },
   methods: {
-    more(){
-      // axios 요청하기
-      // 문자열 안에 변수 설정 방법(es6)
-      axios.get(`https://minjaehyun.github.io/vue/more${this.count++}.json`)
-      .then(result =>{
-        // console.log(result.data);
-        // 게시글 저장하기
-        this.post.push(result.data)
-      })
-      .catch()
-    }
-  }
-}
+    more() {
+      axios
+        .get(`https://minjaehyun.github.io/vue/more${this.count++}.json`)
+        .then((result) => {
+          this.post.push(result.data);
+        })
+        .catch();
+    },
+    nextStep(){
+      this.step = 1
+    },
+  },
+};
 </script>
 
 <style>
-@import 'assets/css/app.css';
+@import "assets/css/app.css";
 </style>
