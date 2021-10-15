c1360ebb90c17787292dadff338ea63b0908e0a8
{krism01} <{krism01@naver.com}>
2021-10-15 11:45:06 +0900

    componenet 기본 구조 구현

---
+++ vuestagram/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="header">
+      <ul class="header-button-left">
+        <li>Cancel</li>
+      </ul>
+      <ul class="header-button-right">
+        <li>Next</li>
+      </ul>
+      <img src="./assets/logo.png" class="logo" />
+    </div>
+
+    <Container />
+
+    <div class="footer">
+      <ul class="footer-button-plus">
+        <input type="file" id="file" class="inputfile" />
+        <label for="file" class="input-plus">+</label>
+      </ul>
+  </div>
+</template>
+
+<script>
+import Container from './components/Container.vue'
+
+export default {
+  name: 'App',
+  components: { Container, }
+}
+</script>
+
+<style>
+@import 'assets/css/app.css';
+</style>
