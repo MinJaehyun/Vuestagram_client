f1a916e135bd036336e812c81f9471c926bfacc7
{krism01} <{krism01@naver.com}>
2022-03-15 00:37:21 +0900

    모달 컴포넌트 생성 및 닫기 버튼 기능 구현

---
+++ vuestagram/src/components/shareSuccessPage.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    <div class="black-bg">
+      <div class="white-bg ">
+        <div class="flex">
+          <img src="../assets/image/10a8cbeb94ba.gif" alt="애니메이션 효과" />
+        </div>
+        <p class="flex">게시물이 공유되었습니다:)</p>
+        <!-- modal 을 false 로 변경한다. -->
+        <button
+          style="margin:auto; display:block;"
+          type="button"
+          @click="$emit('modal')"
+        >
+          닫기
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {};
+</script>
+
+<style>
+body {
+  margin: 0;
+}
+div {
+  box-sizing: border-box;
+}
+.black-bg {
+  width: 24%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.5);
+  /* position: static; */
+  position: fixed;
+  padding: 20px;
+}
+.white-bg {
+  width: 100%;
+  background: white;
+  border-radius: 8px;
+  padding: 20px;
+  color: grey;
+}
+.flex {
+  display: flex;
+  justify-content: center;
+}
+</style>
