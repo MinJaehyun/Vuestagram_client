71fa019cc9dd3df6eb56b435f03adc89a083d55b
{krism01} <{krism01@naver.com}>
2022-03-15 11:40:42 +0900

    fix and update 1. white-bg 에 닫기 기능 추가 2. 이미지 뒤에 모달창 뜨는 문제점 해결 3. 브라우저 크기 조절 시, 모달창 크기 줄어드는 문제점 해결

--- vuestagram/src/components/shareSuccessPage.vue
+++ vuestagram/src/components/shareSuccessPage.vue
@@ -1,17 +1,12 @@
 <template>
   <div>
     <div class="black-bg">
-      <div class="white-bg ">
+      <div class="white-bg " @click="$emit('modal')">
         <div class="flex">
           <img src="../assets/image/10a8cbeb94ba.gif" alt="애니메이션 효과" />
         </div>
         <p class="flex">게시물이 공유되었습니다:)</p>
-        <!-- modal 을 false 로 변경한다. -->
-        <button
-          style="margin:auto; display:block;"
-          type="button"
-          @click="$emit('modal')"
-        >
+        <button style="margin:auto; display:block;" type="button">
           닫기
         </button>
       </div>
@@ -30,14 +25,22 @@ div {
   box-sizing: border-box;
 }
 .black-bg {
-  width: 24%;
+  /* z-index 맨 앞에 위치하게 설정하여, 이미지 뒤로 모달창이 설정되는 문제점을 해결! */
+  z-index: 500;
+  width: auto;
+  /* width: 24%; */
   height: 100%;
   background: rgba(0, 0, 0, 0.5);
-  /* position: static; */
   position: fixed;
-  padding: 20px;
+  padding: 108px;
 }
 .white-bg {
+  /* 브라우저 크기 조절 시, 이미지 크기에 맞게 조절하지 못하는 문제점
+   */
+  /* TODO: position 지정 방법? */
+  /* position: sticky; */
+  position: relative;
+  top: 50%;
   width: 100%;
   background: white;
   border-radius: 8px;
