975d4021fc0a7b3d830b8103b9d9edaae5363798
{krism01} <{krism01@naver.com}>
2022-03-16 13:57:53 +0900

    feature Explain component

---
+++ vuestagram/src/components/Explain.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="explain">
+    <h4>이모티콘을 눌러 멋진 게시물을 만들어 주세요 :)</h4>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Explain",
+  // FIXME: vue 이므로 mounted() 사용하여 처리, nuxt 로 리펙토링할 시, asyncData 또는 fetch 적용한다
+  mounted() {
+    this.$store.dispatch("HELLO_SET_TIMEOUT");
+  },
+};
+</script>
+
+<style>
+@import "../assets/css/explain.css";
+</style>
