1d58a11e86ee65631858a62cbe4dad008597db0b
{krism01} <{krism01@naver.com}>
2022-03-22 18:35:49 +0900

    Header 적용하기 위해 app 컴포넌트 분리

---
+++ vuestagram/src/components/Section.vue
@@ -0,0 +1,153 @@
+<template>
+  <div>
+    <div>
+      <Explain v-if="$store.state.visit == true" />
+      <transition name="fade">
+        <ShareSuccessPage v-if="modal == true" @modal="modal = false" />
+      </transition>
+    </div>
+    <div class="app">
+      <div>
+        <div v-if="step == 0">
+          <div class="footer ">
+            <ul class="footer-button-plus ">
+              <input @change="upload" type="file" id="file" class="inputfile" />
+              <label for="file" class="input-plus btn btn-outline-success"
+                >make&#9997;</label
+              >
+            </ul>
+          </div>
+        </div>
+
+        <div class="header">
+          <!-- step == 1 -->
+          <div v-if="step == 1">
+            <ul class="header-button-left" @click="step--">
+              <li style="color: black">Cancel</li>
+            </ul>
+
+            <ul class="header-button-right" @click="step++">
+              <li style="color: black; box-sizing: none;">Next</li>
+            </ul>
+
+            <!-- TODO: 붓 이모티콘은 사용하지 않으므로 기능 추가 시, 설정한다! -->
+            <!-- <div style="font-size: 40px; text-align:center;">&#128396;</div> -->
+          </div>
+
+          <!-- step == 2 -->
+          <div v-if="step == 2">
+            <ul class="header-button-right">
+              <li @click="publish" style="color: black">발행</li>
+            </ul>
+          </div>
+        </div>
+
+        <!-- vuex 요청한 게시글 더 보기 내용 출력 -->
+        <!-- {{$store.state.post[0].name}} -->
+
+        <!-- component -->
+        <Container
+          :selectFilter="selectFilter"
+          :post="post"
+          :step="step"
+          :url="url"
+          @uploadText="content = $event"
+        />
+
+        <div>
+          <!-- axios 요청하여 깃헙 서버에서 json 가져오기 -->
+          <div v-if="step == 0" class="container ">
+            <button @click="more" class="btn btn-outline-success">
+              <!-- <button @click="$store.dispatch('getData')"> -->
+              게시글 더보기
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import Container from "../components/Container.vue";
+import ShareSuccessPage from "../components/shareSuccessPage.vue";
+import Explain from "../components/Explain.vue";
+import post from "../assets/data/post";
+import axios from "axios";
+
+export default {
+  name: "SectionApp",
+  components: { Container, ShareSuccessPage, Explain },
+  data() {
+    return {
+      content: "",
+      url: "",
+      step: 0,
+      post: post,
+      count: 0,
+      selectFilter: "",
+      modal: false,
+    };
+  },
+  mounted() {
+    this.emitter.on("clickBox", (result) => {
+      this.selectFilter = result; // selectFilter 에는 _1977 들어있는 상태
+    });
+  },
+  methods: {
+    async more() {
+      try {
+        const post = await axios.get(
+          // `https://minjaehyun.github.io/vue/more${this.count++}.json`
+          `https://minjaehyun.github.io/vuestagram_ref/vuestagram/src/assets/data/more${this
+            .count++}.json`
+
+          // FIXME: 추 후 리펙토링 하기
+          // 여러개의 데이터가 담긴 객체배열에 요청 시, 3개씩 가져오도록 코드 작성하기
+          // `https://minjaehyun.github.io/vuestagram_ref/vuestagram/src/assets/data/more.json`
+          // this.post.push(post.data);
+        );
+        // 3개의 post.data 를 concat 을 사용하여 post 에 담는다
+        // concat 으로 배열안에 배열을 풀어서 넣을 수 있다.
+        this.post = this.post.concat(post.data);
+      } catch (error) {
+        console.log(error.response);
+        alert("더 이상 게시물이 존재하지 않습니다.");
+        // confirm("");
+      }
+    },
+    upload(e) {
+      let files = e.target.files;
+      let file = files[0];
+      let url = URL.createObjectURL(file);
+      this.url = url;
+      this.step = 1;
+    },
+    publish() {
+      var uploadPost = {
+        name: "tester",
+        userImage: "https://placeimg.com/100/100/arch",
+        postImage: this.url,
+        likes: 0,
+        // 날짜와 시간 출력
+        date: new Date().toLocaleString(),
+        liked: false,
+        content: this.content,
+        filter: this.selectFilter,
+      };
+      this.post.unshift(uploadPost);
+      this.step = 0;
+      this.modal = true;
+    },
+  },
+};
+</script>
+<style>
+@import "../assets/css/app.css";
+@import "../assets/css/fade.css";
+
+.container {
+  display: flex;
+  justify-content: center;
+  margin: 2rem 0;
+}
+</style>
