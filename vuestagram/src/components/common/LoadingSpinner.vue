4335446a6c08541c7212179f1695529df74bd69b
{krism01} <{krism01@naver.com}>
2022-04-02 15:02:11 +0900

    Add spinner css animation

---
+++ vuestagram/src/components/common/LoadingSpinner.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="spinner-container">
+    <div class="spinner"></div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.spinner-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100px;
+}
+.spinner {
+  width: 50px;
+  height: 50px;
+  bottom: 60px;
+  left: 97px;
+  border-radius: 50%;
+  border: 5px solid #e0e0e0;
+  border-top-color: #f19022;
+  border-bottom-color: #f19022;
+  animation: spin 1s linear infinite;
+  position: relative;
+}
+@keyframes spin {
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(180deg);
+  }
+}
+</style>
