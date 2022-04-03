fb07973e832b1cb2b6a15542e7cf1a3a61e82197
{krism01} <{krism01@naver.com}>
2022-04-03 23:52:13 +0900

    fortawesome with vue3 및 주석 추가

--- vuestagram/src/main.js
+++ vuestagram/src/main.js
@@ -3,12 +3,28 @@ import App from './App.vue'
 import store from './store';
 import mitt from 'mitt';
 
+// fortawesome setup
+import { library as faLibrary } from '@fortawesome/fontawesome-svg-core';
+import { faBuildingLock, faCodeBranch, faCamera } from '@fortawesome/free-solid-svg-icons';
+import { faInstagram, faTwitter, faFacebook, faStackOverflow, faGithub } from '@fortawesome/free-brands-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+
+// bootstrap setup
 import "bootstrap/dist/css/bootstrap.min.css"
 import 'bootstrap/dist/js/bootstrap.esm.js'
 import "bootstrap"
 
+
+// emitter 전역 설정
 let emitter = mitt();
-let app = createApp(App)
 app.config.globalProperties.emitter = emitter;
 
-app.use(store).mount('#app');
+// createApp
+let app = createApp(App)
+
+// fortawesome setup
+faLibrary.add(faInstagram, faTwitter, faFacebook, faStackOverflow, faGithub, faBuildingLock, faCodeBranch, faCamera)
+app.component('font-awesome-icon', FontAwesomeIcon)
+
+app.use(store)
+app.mount('#app');
