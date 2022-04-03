b5fbf8ecf2f642d28207ea0972fab7c6af72cb6a
{krism01} <{krism01@naver.com}>
2022-04-03 23:53:16 +0900

    update 1. 불필요한 코드 제거 2. css 분리

--- vuestagram/src/components/FilterBox.vue
+++ vuestagram/src/components/FilterBox.vue
@@ -5,8 +5,6 @@
     class="filter-item"
     :style="{ backgroundImage: `url(${url})` }"
   >
-    <!-- Container.vue 에서 v-slot 사용하여 데이터 내려줄 시 아래 slot 으로 받을 수 있다 -->
-    <!-- <slot name="a"></slot> -->
     {{ filter }}
   </div>
 </template>
@@ -20,21 +18,12 @@ export default {
   },
   methods: {
     fire() {
+      // NOTE: emitter 사용하여, depth 가 2단계 이상이거나 같은 depth 라인에게 값을 넘길 수 있다.
       this.emitter.emit("clickBox", this.filter);
     },
   },
 };
 </script>
-
 <style>
-.filter-item {
-  width: 100px;
-  height: 100px;
-  margin: 10px 10px 10px auto;
-  padding: 8px;
-  display: inline-block;
-  color: white;
-  background-size: cover;
-  background-position: center;
-}
+@import "../assets/css/filterBox.css";
 </style>
