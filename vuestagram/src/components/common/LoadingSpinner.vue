eec29cafd32c073519c047d0d89497c9de82e170
{krism01} <{krism01@naver.com}>
2022-04-03 23:54:30 +0900

    css 변경

--- vuestagram/src/components/common/LoadingSpinner.vue
+++ vuestagram/src/components/common/LoadingSpinner.vue
@@ -10,22 +10,22 @@ export default {};
 
 <style scoped>
 .spinner-container {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  height: 100px;
+  /* display: flex; */
+  /* justify-content: center; */
+  /* align-items: center; */
+  /* height: 100px; */
 }
 .spinner {
   width: 50px;
   height: 50px;
-  bottom: 60px;
-  left: 97px;
+  position: relative;
+  top: 42px;
+  left: 53px;
   border-radius: 50%;
   border: 5px solid #e0e0e0;
   border-top-color: #f19022;
   border-bottom-color: #f19022;
   animation: spin 1s linear infinite;
-  position: relative;
 }
 @keyframes spin {
   from {
