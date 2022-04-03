d04ffae86dd575260c4d55129db34a7548226530
{krism01} <{krism01@naver.com}>
2022-04-03 23:49:36 +0900

    css 분리

--- vuestagram/src/components/Container.vue
+++ vuestagram/src/components/Container.vue
@@ -1,11 +1,11 @@
 <template>
   <div>
-    <!-- post page -->
     <div v-if="step == 0">
+      <!-- post step 0 -->
       <Post :post="item" v-for="item in post" :key="item.id" />
     </div>
 
-    <!-- 필터선택 page -->
+    <!-- 필터선택 step 1 -->
     <div v-if="step == 1">
       <div
         :class="selectFilter"
@@ -19,14 +19,11 @@
           :url="url"
           v-for="filter in filterList"
           :key="filter.id"
-        >
-          <!-- {{ filter }} 는 slot 에 내려줄 데이터 -->
-          <!-- <template v-slot:a><span>{{ filter }}</span></template> -->
-        </FilterBox>
+        />
       </div>
     </div>
 
-    <!-- 글 작성 page -->
+    <!-- 글 작성 step 2 -->
     <div v-if="step == 2">
       <div
         :class="selectFilter"
@@ -67,46 +64,5 @@ export default {
 </script>
 
 <style>
-.upload-image {
-  width: 100%;
-  height: 350px;
-  background: cornflowerblue;
-  background-size: cover;
-}
-.filters {
-  overflow-x: scroll;
-  white-space: nowrap;
-}
-.filter-1 {
-  width: 100px;
-  height: 100px;
-  background-color: cornflowerblue;
-  margin: 10px 10px 10px auto;
-  padding: 8px;
-  display: inline-block;
-  color: white;
-  background-size: cover;
-}
-.filters::-webkit-scrollbar {
-  height: 5px;
-}
-.filters::-webkit-scrollbar-track {
-  background: #f1f1f1;
-}
-.filters::-webkit-scrollbar-thumb {
-  background: #888;
-  border-radius: 5px;
-}
-.filters::-webkit-scrollbar-thumb:hover {
-  background: #555;
-}
-.write-box {
-  border: none;
-  width: 90%;
-  height: 100px;
-  padding: 15px;
-  margin: auto;
-  display: block;
-  outline: none;
-}
+@import "../assets/css/container.css";
 </style>
