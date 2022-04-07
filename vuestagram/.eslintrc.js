38794288fed80853df251c3af2264f6f40d1f5b1
{krism01} <{krism01@naver.com}>
2022-04-07 15:05:22 +0900

    package.json 에서 분리하여 .eslintrc.js 설정

---
+++ vuestagram/.eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+	env: {
+		browser: true,
+		es6: true,
+		node: true,
+	},
+	extends: ['@vue/prettier', 'plugin:vue/essential'],
+	globals: {
+		Atomics: 'readonly',
+		SharedArrayBuffer: 'readonly',
+	},
+	parserOptions: {
+		ecmaVersion: 2018,
+		sourceType: 'module',
+	},
+	plugins: ['vue'],
+	rules: {
+		'no-console': 'off',
+		'prettier/prettier': [
+			'warn',
+			{
+				singleQuote: true,
+				semi: true,
+				useTabs: true,
+				tabWidth: 2,
+				trailingComma: 'all',
+				printWidth: 80,
+				bracketSpacing: true,
+				arrowParens: 'avoid',
+			},
+		],
+	},
+};
