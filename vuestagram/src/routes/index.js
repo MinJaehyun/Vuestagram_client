dc7d0ee0c023b2de13ed7079abf0fa6151a5c49e
{krism01} <{krism01@naver.com}>
2022-04-07 23:52:39 +0900

    Add login, signup router

--- vuestagram/src/routes/index.js
+++ vuestagram/src/routes/index.js
@@ -1,14 +1,29 @@
 // vue-router 라이브러리 { createWebHistory, createRouter } 사용
 import { createWebHistory, createRouter } from 'vue-router';
-// import Signup from '@/components/Signup.vue';
-import Signup from '../components/Signup.vue';
+import Signup from '@/views/Signup.vue';
+import Login from '@/views/Login.vue';
+// import ErrorPage from '@/views/ErrorPage.vue';
+// import Section from '@/components/Section.vue';
 
 // routes 설정
 const routes = [
+  // {
+  //   path: '/',
+  //   component: Section,
+  //   redirect: '/login',
+  // },
   {
     path: '/signup',
     component: Signup,
   },
+  {
+    path: '/login',
+    component: Login,
+  },
+  // {
+  //   path: '/:anything(.*)',
+  //   component: ErrorPage,
+  // },
 ];
 
 //  위에서 설정한 routes 경로 가져와서 라이브러리 적용
