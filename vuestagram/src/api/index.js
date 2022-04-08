bcd0ca908c6c3cb7b20971cf345fe89f4b64a742
{krism01} <{krism01@naver.com}>
2022-04-08 22:42:20 +0900

    test axios.post in Heroku-server

--- vuestagram/src/api/index.js
+++ vuestagram/src/api/index.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 function registerUser(userData) {
-  const url = 'http://localhost:3000/signup';
+  // const url = 'http://localhost:3000/signup';
+  const url = 'https://real-vuestagram-server.herokuapp.com';
   return axios.post(url, userData);
 }
 
