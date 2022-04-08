6e75650584d6cd10fd550208b7e022d6eed99a65
{krism01} <{krism01@naver.com}>
2022-04-08 12:07:52 +0900

    로그인, 회원가입 page 생성

---
+++ vuestagram/src/views/LoginPage.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <LoginForm />
+  </div>
+</template>
+
+<script>
+import LoginForm from '../components/account/LoginForm.vue';
+export default {
+  components: { LoginForm },
+};
+</script>
+
+<style></style>
