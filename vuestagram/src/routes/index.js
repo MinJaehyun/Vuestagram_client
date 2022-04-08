d24c501f99bc1c419e7cc0ac249dbf581fdc087a
{krism01} <{krism01@naver.com}>
2022-04-08 12:05:25 +0900

    파일 이름 변경

--- vuestagram/src/routes/index.js
+++ vuestagram/src/routes/index.js
@@ -1,7 +1,7 @@
 // vue-router 라이브러리 { createWebHistory, createRouter } 사용
 import { createWebHistory, createRouter } from 'vue-router';
-import Signup from '@/views/Signup.vue';
-import Login from '@/views/Login.vue';
+import SignupPage from '@/views/SignupPage.vue';
+import LoginPage from '@/views/LoginPage.vue';
 // import ErrorPage from '@/views/ErrorPage.vue';
 // import Section from '@/components/Section.vue';
 
@@ -14,11 +14,11 @@ const routes = [
   // },
   {
     path: '/signup',
-    component: Signup,
+    component: SignupPage,
   },
   {
     path: '/login',
-    component: Login,
+    component: LoginPage,
   },
   // {
   //   path: '/:anything(.*)',
