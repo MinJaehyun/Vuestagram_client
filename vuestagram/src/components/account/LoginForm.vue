236fef991e95b68f2ee0dfc513730e3c6f119f2e
{krism01} <{krism01@naver.com}>
2022-04-08 12:07:06 +0900

    test form

---
+++ vuestagram/src/components/account/LoginForm.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>login test</div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
---
+++ vuestagram/src/components/account/SignupForm.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>TODO: Form 작성하기</div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
