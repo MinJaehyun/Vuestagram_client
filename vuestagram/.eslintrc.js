d91d35a42a6e744839fb8adec1095ceb6c8a2fa6
{krism01} <{krism01@naver.com}>
2022-04-11 09:32:28 +0900

    elint 변경

--- vuestagram/.eslintrc.js
+++ vuestagram/.eslintrc.js
@@ -21,8 +21,6 @@ module.exports = {
       {
         singleQuote: true,
         semi: true,
-        // useTabs: true,
-        // tabWidth: 2,
         trailingComma: 'all',
         printWidth: 90,
         bracketSpacing: true,
