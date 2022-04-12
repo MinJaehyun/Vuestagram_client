9dbc4c1437c6e38990e0649d2364c87ed63713fb
{krism01} <{krism01@naver.com}>
2022-04-12 17:24:25 +0900

    로고 위치 변경

--- vuestagram/src/components/common/AppHeader.vue
+++ vuestagram/src/components/common/AppHeader.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
     <!-- FIXME: to="" 문자열만 받도록 되어 있는데, api/index.js 주소를 가져오는게 효율적이다. -->
-    <router-link @click="$emit('appNone')" to="/auth/signup">Signup</router-link>
     <router-link @click="$emit('appNone')" to="/auth/login">Login</router-link>
+    <router-link @click="$emit('appNone')" to="/auth/signup">Signup</router-link>
   </div>
 </template>
 
