eff024872beb9abda4f3bd2aa1fc26bcd5609179
{krism01} <{krism01@naver.com}>
2022-04-13 10:35:09 +0900

    로그인 했을 때, 헤더 분기 처리

--- vuestagram/src/components/common/AppHeader.vue
+++ vuestagram/src/components/common/AppHeader.vue
@@ -1,13 +1,28 @@
 <template>
   <div>
-    <!-- FIXME: to="" 문자열만 받도록 되어 있는데, api/index.js 주소를 가져오는게 효율적이다. -->
-    <router-link @click="$emit('appNone')" to="/auth/login">Login</router-link>
-    <router-link @click="$emit('appNone')" to="/auth/signup">Signup</router-link>
+    <!-- 로그인 시 -->
+    <!-- store 에 isLogin 값을 가져온다. true -->
+    <template v-if="isUserLogin">
+      <span>{{ $store.state.username }}</span>
+      <router-link @click="$emit('appNone')" to="/auth/logout">logout</router-link>
+    </template>
+    <!-- 로그아웃 시 -->
+    <template v-else>
+      <!-- FIXME: to="" 문자열만 받도록 되어 있는데, api/index.js 주소를 가져오는게 효율적이다. -->
+      <router-link @click="$emit('appNone')" to="/auth/login">Login</router-link>
+      <router-link @click="$emit('appNone')" to="/auth/signup">Signup</router-link>
+    </template>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  computed: {
+    isUserLogin() {
+      return this.$store.getters.isLogin;
+    },
+  },
+};
 </script>
 
 <style></style>
