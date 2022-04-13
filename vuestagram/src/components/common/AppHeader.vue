d264584e57dec8dc711ff8c865f9d6998c14e29e
{krism01} <{krism01@naver.com}>
2022-04-13 22:41:43 +0900

    AppHeader 에 들어갈 요소들 모으고, router-view 구조 적용하면서 불필요한  제거

--- vuestagram/src/components/common/AppHeader.vue
+++ vuestagram/src/components/common/AppHeader.vue
@@ -1,33 +1,30 @@
 <template>
   <div class="navbar">
     <div class="navbar__logo">
-      <router-link to="/" @click="appDisplay" class="gradient">Vuestagram</router-link>
+      <router-link to="/" class="gradient">Vuestagram</router-link>
     </div>
     <ul style="display: flex; margin: 8px; padding: 8px 12px; line-height: 0px">
       <li>
         <!-- home icon -->
-        <router-link to="/" @click="appDisplay">
+        <router-link to="/">
           <ion-icon size="large" name="home-outline"></ion-icon>
         </router-link>
       </li>
       <li>
         <!-- search icon -->
-        <ion-icon @click="comingSoonFunction" size="large" name="search-outline">
-        </ion-icon>
+        <ion-icon @click="comingSoonAlert" size="large" name="search-outline"> </ion-icon>
       </li>
     </ul>
     <div class="navbar__account gradient" style="padding: 8px 12px">
-      <!-- 로그인 시 -->
-      <!-- store 에 isLogin 값을 가져온다. true -->
+      <!-- 로그인: store 에 isLogin 값을 가져온다 (true) -->
       <template v-if="isUserLogin">
         <span>{{ $store.state.username }}</span>
-        <router-link @click="$emit('appNone')" to="/auth/logout">logout</router-link>
+        <router-link to="/auth/logout">logout</router-link>
       </template>
-      <!-- 로그아웃 시 -->
+      <!-- 로그아웃 -->
       <template v-else>
-        <!-- FIXME: to="" 문자열만 받도록 되어 있는데, api/index.js 주소를 가져오는게 효율적이다. -->
-        <router-link @click="$emit('appNone')" to="/auth/login">Login</router-link>
-        <router-link @click="$emit('appNone')" to="/auth/signup">Signup</router-link>
+        <router-link to="/auth/login">Login</router-link>
+        <router-link to="/auth/signup">Signup</router-link>
       </template>
     </div>
 
@@ -43,6 +40,11 @@ export default {
   computed: {
     isUserLogin() {
       return this.$store.getters.isLogin;
+    },
+  },
+  methods: {
+    comingSoonAlert() {
+      alert('해당 기능은 준비 중입니다 :)');
     },
   },
 };
