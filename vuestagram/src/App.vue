88a834e4718766ff1c471027f5ecd568b90f399e
{krism01} <{krism01@naver.com}>
2022-04-13 21:36:37 +0900

    최상단 App 에서 AppHeader 와 Post 를 가져오는 구조로 컴포넌트 개선

--- vuestagram/src/App.vue
+++ vuestagram/src/App.vue
@@ -3,19 +3,19 @@
     <transition name="fade">
       <ShareSuccessPage v-if="$store.state.modal == true" @modal="modal = false" />
     </transition>
-    <section>
-      <Section />
-    </section>
+    <AppHeader />
+    <Section />
   </div>
 </template>
 
 <script>
 import Section from './components/Section.vue';
 import ShareSuccessPage from './components/shareSuccessPage.vue';
+import AppHeader from './components/common/AppHeader.vue';
 
 export default {
   name: 'App',
-  components: { Section, ShareSuccessPage },
+  components: { Section, ShareSuccessPage, AppHeader },
 };
 </script>
 
