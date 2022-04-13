7d7f5ecf3b218fd5331d012f53910517ebbaa3c2
{krism01} <{krism01@naver.com}>
2022-04-13 22:41:58 +0900

    router setup

--- vuestagram/src/routes/index.js
+++ vuestagram/src/routes/index.js
@@ -2,17 +2,19 @@
 import { createWebHistory, createRouter } from 'vue-router';
 import SignupPage from '@/views/SignupPage.vue';
 import LoginPage from '@/views/LoginPage.vue';
-import Container from '@/components/Container.vue';
 import LogoutPage from '@/views/LogoutPage';
 import Section from '@/components/Section.vue';
-// import MainPage from '@/views/MainPage';
-// import ErrorPage from '@/views/ErrorPage.vue';
+import ErrorPage from '@/views/ErrorPage.vue';
 
 // routes 설정
 const routes = [
   {
     path: '/',
-    component: Container,
+    component: Section,
+  },
+  {
+    path: '/main',
+    component: Section,
   },
   {
     path: '/auth/signup',
@@ -27,13 +29,9 @@ const routes = [
     component: LogoutPage,
   },
   {
-    path: '/main',
-    component: Section,
+    path: '/:anything(.*)',
+    component: ErrorPage,
   },
-  // {
-  //   path: '/:anything(.*)',
-  //   component: ErrorPage,
-  // },
 ];
 
 //  위에서 설정한 routes 경로 가져와서 라이브러리 적용
