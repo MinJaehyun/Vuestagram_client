01732cd8a06b2b0088c06171f1c666eed2b19f44
{krism01} <{krism01@naver.com}>
2022-04-13 23:57:25 +0900

    600px 이하에서 햄버거 메뉴 적용

--- vuestagram/src/components/common/AppHeader.vue
+++ vuestagram/src/components/common/AppHeader.vue
@@ -3,7 +3,7 @@
     <div class="navbar__logo">
       <router-link to="/" class="gradient">Vuestagram</router-link>
     </div>
-    <ul style="display: flex; margin: 8px; padding: 8px 12px; line-height: 0px">
+    <ul style="margin: 8px; line-height: 0px" class="navbar__menu">
       <li>
         <!-- home icon -->
         <router-link to="/">
@@ -18,17 +18,18 @@
     <div class="navbar__account gradient" style="padding: 8px 12px">
       <!-- 로그인: store 에 isLogin 값을 가져온다 (true) -->
       <template v-if="isUserLogin">
-        <span>{{ $store.state.username }}</span>
+        <span>{{ $store.state.username }}</span> |
         <router-link to="/auth/logout">logout</router-link>
       </template>
       <!-- 로그아웃 -->
       <template v-else>
-        <router-link to="/auth/login">Login</router-link>
+        <router-link to="/auth/login">Login</router-link> |
         <router-link to="/auth/signup">Signup</router-link>
       </template>
     </div>
 
     <!-- navbar__toggleBtn -->
+    <!-- @click="$emit('toggleBtn', isToggleBtn) -->
     <a href="#" class="navbar__toggleBtn" @click="toggleBtn(isToggleBtn)">
       <font-awesome-icon icon="bars" />
     </a>
@@ -37,6 +38,11 @@
 
 <script>
 export default {
+  data() {
+    return {
+      isToggleBtn: false,
+    };
+  },
   computed: {
     isUserLogin() {
       return this.$store.getters.isLogin;
@@ -45,6 +51,16 @@ export default {
   methods: {
     comingSoonAlert() {
       alert('해당 기능은 준비 중입니다 :)');
+    },
+    toggleBtn(isToggleBtn) {
+      this.isToggleBtn = !isToggleBtn;
+      // 버튼(홈, 검색)
+      const menu = document.querySelector('.navbar__menu');
+      // 계정(아이디, 로그아웃, 로그인, 회원가입)
+      const account = document.querySelector('.navbar__account');
+      /* Section.vue 의 toggleBtn() 함수를 실행하여, app.css 의 menu 와 account 를 classList.toggle("active") 설정 */
+      menu.classList.toggle('active');
+      account.classList.toggle('active');
     },
   },
 };
