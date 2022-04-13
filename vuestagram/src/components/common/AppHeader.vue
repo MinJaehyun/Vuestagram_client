d53e0bfd8d00af385352864f2d1ec7a2e9deb597
{krism01} <{krism01@naver.com}>
2022-04-13 21:42:44 +0900

    AppHeader 요소 가져옴

--- vuestagram/src/components/common/AppHeader.vue
+++ vuestagram/src/components/common/AppHeader.vue
@@ -1,17 +1,40 @@
 <template>
-  <div>
-    <!-- 로그인 시 -->
-    <!-- store 에 isLogin 값을 가져온다. true -->
-    <template v-if="isUserLogin">
-      <span>{{ $store.state.username }}</span>
-      <router-link @click="$emit('appNone')" to="/auth/logout">logout</router-link>
-    </template>
-    <!-- 로그아웃 시 -->
-    <template v-else>
-      <!-- FIXME: to="" 문자열만 받도록 되어 있는데, api/index.js 주소를 가져오는게 효율적이다. -->
-      <router-link @click="$emit('appNone')" to="/auth/login">Login</router-link>
-      <router-link @click="$emit('appNone')" to="/auth/signup">Signup</router-link>
-    </template>
+  <div class="navbar">
+    <div class="navbar__logo">
+      <router-link to="/" @click="appDisplay" class="gradient">Vuestagram</router-link>
+    </div>
+    <ul style="display: flex; margin: 8px; padding: 8px 12px; line-height: 0px">
+      <li>
+        <!-- home icon -->
+        <router-link to="/" @click="appDisplay">
+          <ion-icon size="large" name="home-outline"></ion-icon>
+        </router-link>
+      </li>
+      <li>
+        <!-- search icon -->
+        <ion-icon @click="comingSoonFunction" size="large" name="search-outline">
+        </ion-icon>
+      </li>
+    </ul>
+    <div class="navbar__account gradient" style="padding: 8px 12px">
+      <!-- 로그인 시 -->
+      <!-- store 에 isLogin 값을 가져온다. true -->
+      <template v-if="isUserLogin">
+        <span>{{ $store.state.username }}</span>
+        <router-link @click="$emit('appNone')" to="/auth/logout">logout</router-link>
+      </template>
+      <!-- 로그아웃 시 -->
+      <template v-else>
+        <!-- FIXME: to="" 문자열만 받도록 되어 있는데, api/index.js 주소를 가져오는게 효율적이다. -->
+        <router-link @click="$emit('appNone')" to="/auth/login">Login</router-link>
+        <router-link @click="$emit('appNone')" to="/auth/signup">Signup</router-link>
+      </template>
+    </div>
+
+    <!-- navbar__toggleBtn -->
+    <a href="#" class="navbar__toggleBtn" @click="toggleBtn(isToggleBtn)">
+      <font-awesome-icon icon="bars" />
+    </a>
   </div>
 </template>
 
@@ -25,4 +48,7 @@ export default {
 };
 </script>
 
-<style></style>
+<style>
+@import '../../assets/css/app.css';
+@import '../../assets/css/gradient.css';
+</style>
