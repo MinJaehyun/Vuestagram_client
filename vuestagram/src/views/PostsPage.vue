77afbd1b39d2e58ba0b5d6965ae361e902dc42fe
{krism01} <{krism01@naver.com}>
2022-04-16 22:09:33 +0900

    views PostsPage 설정

--- vuestagram/src/views/PostsPage.vue
+++ vuestagram/src/views/PostsPage.vue
@@ -1,9 +1,41 @@
 <template>
-  <div>Post Page</div>
+  <div class="list-container contents">
+    <h1 class="page-header page-header-wrap">학습 노트</h1>
+    <LoadingSpinner v-if="isLoading"></LoadingSpinner>
+    <!-- ul 로 views/ 안에 components 를 감싼다 -->
+    <ul v-else>
+      <PostPage v-for="post in posts" :key="post._id" :post="post"></PostPage>
+    </ul>
+  </div>
 </template>
 
 <script>
-export default {};
+import PostPage from '../components/posts/PostList.vue';
+import { fetchPost } from '@/api/index';
+import LoadingSpinner from '@/components/common/LoadingSpinner.vue';
+
+export default {
+  components: { PostPage, LoadingSpinner },
+  data() {
+    return {
+      posts: [],
+      isLoading: false,
+    };
+  },
+  methods: {
+    async fetchData() {
+      const { data } = await fetchPost();
+      this.posts = data.post;
+    },
+  },
+  created() {
+    this.isLoading = true;
+    this.fetchData();
+    this.isLoading = false;
+  },
+};
 </script>
 
-<style></style>
+<style>
+@import '../assets/css/posts/common.css';
+</style>
