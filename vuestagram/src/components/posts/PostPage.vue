e86b72e224c4de3e23edc28a1583452a10dd1cef
{krism01} <{krism01@naver.com}>
2022-04-16 22:07:28 +0900

    PostPage 설정

---
+++ vuestagram/src/components/posts/PostPage.vue
@@ -0,0 +1,29 @@
+<template>
+  <!-- ul>li>div 로 감싸고, class 입히기 -->
+  <li>
+    <div class="post-title">
+      {{ post.title }}
+    </div>
+    <div class="post-contents">
+      {{ post.contents }}
+    </div>
+    <div class="post-created_at">
+      {{ post.created_at }}
+    </div>
+  </li>
+</template>
+
+<script>
+export default {
+  props: {
+    post: {
+      type: Object,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style>
+@import '../../assets/css/posts/common.css';
+</style>
