19968f8fbaef4d4fa0cd99ff9c1b050907d4454d
{krism01} <{krism01@naver.com}>
2022-04-16 22:02:49 +0900

    미인증된 user 를 위한 account 설정 및 인증된 user 를 위한 posts 조회 설정

--- vuestagram/src/api/index.js
+++ vuestagram/src/api/index.js
@@ -1,19 +1,44 @@
 import axios from 'axios';
+import { setInterceptors } from './common/interceptor';
 
-const instance = axios.create({
-  baseURL: process.env.VUE_APP_API_URL,
-});
+// user 전용 미인증된 axios
+function createInstance() {
+  return axios.create({
+    baseURL: process.env.VUE_APP_API_URL,
+  });
+}
+const instance = createInstance();
 
+// user 생성
 function registerUser(userData) {
   return instance.post('auth/signup', userData, { credentials: true }).catch(err => {
     return err.response;
   });
 }
 
+// user 로그인
 function loginUser(userData) {
   return instance.post('auth/login', userData).catch(err => {
     return err.response;
   });
 }
 
-export { registerUser, loginUser };
+// 인증된 user 를 위한 posts
+function createInstanceWithAuth() {
+  const instance = axios.create({
+    baseURL: process.env.VUE_APP_API_URL,
+  });
+  return setInterceptors(instance);
+}
+const postsInstance = createInstanceWithAuth();
+
+// post 조회
+function fetchPost() {
+  return postsInstance.get('posts').catch(err => {
+    return err.response;
+  });
+}
+
+// post 생성
+
+export { registerUser, loginUser, fetchPost };
