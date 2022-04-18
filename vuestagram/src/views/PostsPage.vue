6d61b91119acf40c0a75e6857908760ae4c28f5c
{krism01} <{krism01@naver.com}>
2022-04-18 20:01:49 +0900

    미로그인 상태에서 학습노트 생성 버튼 안 보이게 설정

--- vuestagram/src/views/PostsPage.vue
+++ vuestagram/src/views/PostsPage.vue
@@ -3,22 +3,24 @@
     <h1 class="page-header page-header-wrap">학습 노트</h1>
     <!-- 로그인 한 상태면 아래 안 보이도록 설정하기: token 값 있으면 로그인 중인 상태 -->
     <h2 v-if="!$store.state.token" style="text-align: center" class="gradient">
-      로그인 한 user 만 학습 노트를 작성하실 수 있습니다
+      로그인 후 학습 노트를 작성 및 관리하실 수 있습니다
     </h2>
     <LoadingSpinner v-if="isLoading"></LoadingSpinner>
     <!-- ul 로 views/ 안에 components 를 감싼다 -->
     <ul v-else>
       <PostPage v-for="post in posts" :key="post._id" :post="post"></PostPage>
     </ul>
-    <router-link class="create-button" to="/posts/add">
-      <ion-icon name="add-outline"></ion-icon>
-    </router-link>
+    <div v-if="$store.state.username">
+      <router-link class="create-button" to="/posts/add">
+        <ion-icon name="add-outline"></ion-icon>
+      </router-link>
+    </div>
   </div>
 </template>
 
 <script>
 import PostPage from '@/components/posts/PostList.vue';
-import { fetchPost } from '@/api/index';
+import { fetchPost } from '@/api/posts';
 import LoadingSpinner from '@/components/common/LoadingSpinner.vue';
 
 export default {
