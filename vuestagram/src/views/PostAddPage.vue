de0d02aa7ac6ffd2f641b48a392599d91cf8f63f
{krism01} <{krism01@naver.com}>
2022-04-18 14:38:16 +0900

    import 컴포넌트

---
+++ vuestagram/src/views/PostAddPage.vue
@@ -0,0 +1,15 @@
+<template>
+  <!-- 최상단 x-container -->
+  <div class="form-container">
+    <PostAddForm />
+  </div>
+</template>
+
+<script>
+import PostAddForm from '../components/posts/PostAddForm.vue';
+export default {
+  components: { PostAddForm },
+};
+</script>
+
+<style></style>
