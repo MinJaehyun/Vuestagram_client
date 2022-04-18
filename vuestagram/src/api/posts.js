c2ec4aee4d24bd765ba2271b12869462f5c0e034
{krism01} <{krism01@naver.com}>
2022-04-18 23:28:28 +0900

    posts 경로 수정 및 posts delete api 함수 생성

--- vuestagram/src/api/posts.js
+++ vuestagram/src/api/posts.js
@@ -2,16 +2,23 @@ import { postsInstance } from '@/api/index';
 
 // post 조회
 function fetchPost() {
-  return postsInstance.get('posts').catch(err => {
+  return postsInstance.get('').catch(err => {
     return err.response;
   });
 }
 
 // post 생성
 function postCreate(postData) {
-  return postsInstance.post('posts', postData).catch(err => {
+  return postsInstance.post('', postData).catch(err => {
     return err.response;
   });
 }
 
-export { fetchPost, postCreate };
+// posts 삭제
+function postDelete(id) {
+  return postsInstance.delete(id).catch(err => {
+    return err.response;
+  });
+}
+
+export { fetchPost, postCreate, postDelete };
