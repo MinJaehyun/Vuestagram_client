6932742c9cde9be52bd1081d07648339a9c02e92
{krism01} <{krism01@naver.com}>
2022-08-18 23:39:51 +0900

    개인노트 사용법 공지

--- vuestagram/src/views/PostsPage.vue
+++ vuestagram/src/views/PostsPage.vue
@@ -1,9 +1,12 @@
 <template>
   <div class="list-container contents">
     <h1 class="page-header page-header-wrap"></h1>
-    <!-- 로그인 한 상태면 아래 안 보이도록 설정하기: token 값 있으면 로그인 중인 상태 -->
+    <!-- 로그인 한 상태면 개인 노트 안 보이게 설정: token 유무로 로그인 상태 판단 -->
     <h2 v-if="$store.state.token" style="text-align: center" class="gradient">
-      <!-- 로그인 후 노트를 작성하실 수 있습니다 -->
+      <div class="personal-note-setTimeout">
+        "우측 하단 + 버튼 눌르면 노트를 작성할 수 있습니다"
+      </div>
+      <br />
       개인 노트
     </h2>
     <div v-else style="text-align: center; font-size: 50px" class="gradient">
@@ -47,11 +50,18 @@ export default {
       this.isLoading = false;
       this.posts = data.post;
       // console.log(JSON.parse(JSON.stringify(this.posts)));
-      this.posts.reverse();
+      if (this.posts) this.posts.reverse();
+    },
+    async perNoteFunction() {
+      setTimeout(() => {
+        let personalNote = document.querySelector('.personal-note-setTimeout');
+        if (personalNote) personalNote.style = 'display: none';
+      }, 5000);
     },
   },
   created() {
     this.fetchData();
+    this.perNoteFunction();
   },
 };
 </script>
